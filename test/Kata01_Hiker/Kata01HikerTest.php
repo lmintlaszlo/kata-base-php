46e62246841e0ad427bfac59e6bb4d9cd0ec3115
lmintlaszlo <laszlo.schumann@doclerholding.com>
2014-09-11 19:27:07 +0200

    Hiker folyt kov

--- test/Kata01_Hiker/Kata01HikerTest.php
+++ test/Kata01_Hiker/Kata01HikerTest.php
@@ -5,14 +5,27 @@ use Kata\Kata01Hiker\Hiker;
 
 class Kata01HikerTest extends PHPUnit_Framework_TestCase {
 
-    public function testHiker() {
+    /**
+     * @dataProvider primeDataProvider
+     */
+    public function testHiker()
+    {
         $hiker = new Hiker();
 
         $this->assertEquals(array(2),$hiker->getPrimeFactor(2));
         $this->assertEquals(array(3),$hiker->getPrimeFactor(3));
         $this->assertEquals(array(2,2),$hiker->getPrimeFactor(4));
         $this->assertEquals(array(3,2),$hiker->getPrimeFactor(6));
+        $this->assertEquals(array(3,2,2),$hiker->getPrimeFactor(12));
     }
 
+    public function primeDataProvider()
+    {
+        return array (
+            array()
+        );
+    }
+
+
+
 }
- 
