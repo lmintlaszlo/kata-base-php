f82cabb971d829bc0166d957dbebcdbe8e0c1f87
lmintlaszlo <laszlo.schumann@doclerholding.com>
2014-09-13 13:43:50 +0200

    Add comments and rename getPrimeFactor method to getPrimeFactors

--- src/Kata01PrimeFactors/PrimeFactors.php
+++ src/Kata01PrimeFactors/PrimeFactors.php
@@ -4,12 +4,12 @@ namespace Kata\Kata01PrimeFactors;
 
 class PrimeFactors {
 
-    public function getPrimeFactor($number)
+    public function getPrimeFactors($number)
     {
         $sqrt = sqrt($number);
         for ($i = 2; $i <= $sqrt; $i++) {
             if ($number % $i == 0) {
-                return array_merge(array($i), $this->getPrimeFactor($number/$i));
+                return array_merge(array($i), $this->getPrimeFactors($number/$i));
             }
         }
         return array($number);
--- test/Kata01PrimeFactors/PrimeFactorsTest.php
+++ test/Kata01PrimeFactors/PrimeFactorsTest.php
@@ -6,15 +6,22 @@ use Kata\Kata01PrimeFactors\PrimeFactors;
 class PrimeFactorsTest extends PHPUnit_Framework_TestCase {
 
     /**
+     * Test method for PrimeFactors->getPrimeFactors.
+     *
      * @dataProvider primeDataProvider
      */
     public function testPrimeFactors($number, array $expectedPrimeFactors)
     {
         $primeFactors = new PrimeFactors();
 
-        $this->assertEquals($expectedPrimeFactors, $primeFactors->getPrimeFactor($number));
+        $this->assertEquals($expectedPrimeFactors, $primeFactors->getPrimeFactors($number));
     }
 
+    /**
+     * Data provider for testPrimeFactors.
+     *
+     * @return array
+     */
     public function primeDataProvider()
     {
         return array (
