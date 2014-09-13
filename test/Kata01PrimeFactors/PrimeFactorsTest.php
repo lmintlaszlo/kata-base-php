ebf9fd91f6429862eae2bf5621e4a2a6472d56fe
lmintlaszlo <laszlo.schumann@doclerholding.com>
2014-09-13 13:35:40 +0200

    Using dataprovider

--- test/Kata01PrimeFactors/PrimeFactorsTest.php
+++ test/Kata01PrimeFactors/PrimeFactorsTest.php
@@ -8,21 +8,24 @@ class PrimeFactorsTest extends PHPUnit_Framework_TestCase {
     /**
      * @dataProvider primeDataProvider
      */
-    public function testPrimeFactors()
+    public function testPrimeFactors($number, array $expectedPrimeFactors)
     {
         $primeFactors = new PrimeFactors();
 
-        $this->assertEquals(array(2), $primeFactors->getPrimeFactor(2));
-        $this->assertEquals(array(3), $primeFactors->getPrimeFactor(3));
-        $this->assertEquals(array(2,2), $primeFactors->getPrimeFactor(4));
-        $this->assertEquals(array(2,3), $primeFactors->getPrimeFactor(6));
-        $this->assertEquals(array(2,2,3), $primeFactors->getPrimeFactor(12));
+        $this->assertEquals($expectedPrimeFactors, $primeFactors->getPrimeFactor($number));
     }
 
     public function primeDataProvider()
     {
         return array (
-            array()
+            array(2, array(2)),
+            array(3, array(3)),
+            array(4, array(2,2)),
+            array(5, array(5)),
+            array(6, array(2,3)),
+            array(9, array(3,3)),
+            array(12, array(2,2,3)),
+            array(15, array(3,5)),
         );
     }
 
