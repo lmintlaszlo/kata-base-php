fa096fe40eabcf6cefe9863c7105c4cdb8bcfd68
lmintlaszlo <laszlo.schumann@doclerholding.com>
2014-11-17 23:44:38 +0100

    Validator isValidPassword test

--- test/Homeworks/H06RegistrationApiTest/ValidatorTest.php
+++ test/Homeworks/H06RegistrationApiTest/ValidatorTest.php
@@ -6,9 +6,13 @@ class ValidatorTest extends \PHPUnit_Framework_TestCase
 {
     const USERNAME_VALID                = 'johndoe1';    
     const USERNAME_TOO_SHORT            = 'doe';
-    const USERNAME_CAPITAL_LETTER      = 'JohnDoe';
+    const USERNAME_CAPITAL_LETTER       = 'JohnDoe';
     const USERNAME_ONLY_CAPITAL_LETTERS = 'JOHNDOE';
-    const USERNAME_SPECIAL_CHARACTER   = 'johndoe1_';
+    const USERNAME_SPECIAL_CHARACTER    = 'johndoe1_';
+    
+    const PASSWORD_VALID        = 'asdasd';
+    const PASSWORD_TOO_SHORT    = 'asdas';
+    const PASSWORD_FOR_MISMATCH = 'sasdas';
 
     const RESULT_OK    = true;
     const RESULT_ERROR = false;
@@ -33,22 +37,42 @@ class ValidatorTest extends \PHPUnit_Framework_TestCase
     }
     
     
-    public function testIsValidPassword()
+    /**
+     * @dataProvider isValidPasswordProvider
+     * 
+     * @param bool $expectedResult
+     * @param string $password
+     */
+    public function testIsValidPassword($expectedResult, $password, $passwordConfirm)
     {
-        
+        $this->assertEquals(
+            $expectedResult,
+            $this->validator->isValidPassword($password, $passwordConfirm)
+        );
     }
     
+    
+
     /** Data providers */
     
     public function isValidUsernameProvider()
     {
         return array(
-            array(self::RESULT_OK, self::USERNAME_VALID),
+            array(self::RESULT_OK,    self::USERNAME_VALID),
             array(self::RESULT_ERROR, self::USERNAME_CAPITAL_LETTER),
             array(self::RESULT_ERROR, self::USERNAME_ONLY_CAPITAL_LETTERS),
             array(self::RESULT_ERROR, self::USERNAME_SPECIAL_CHARACTER),
             array(self::RESULT_ERROR, self::USERNAME_TOO_SHORT),
             array(self::RESULT_ERROR, str_repeat(self::USERNAME_VALID, 20)),
+        );
+    }
+    
+    public function isValidPasswordProvider()
+    {
+        return array(
+            array(self::RESULT_OK,    self::PASSWORD_VALID, self::PASSWORD_VALID),
+            array(self::RESULT_ERROR, self::PASSWORD_VALID, self::PASSWORD_FOR_MISMATCH),
+            array(self::RESULT_ERROR, self::PASSWORD_TOO_SHORT, self::PASSWORD_TOO_SHORT),
         );
     }
 }
