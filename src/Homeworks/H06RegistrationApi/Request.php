7d2413a56de46666772e9feaf981b81039d814e1
lmintlaszlo <laszlo.schumann@doclerholding.com>
2014-11-17 23:20:41 +0100

    Validator test username

---
+++ src/Homeworks/H06RegistrationApi/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Kata\Homeworks\H06RegistrationApi;
+
+class Request
+{
+    public $username;
+    public $password;
+    public $passwordConfirm;
+}
---
+++ test/Homeworks/H06RegistrationApiTest/ValidatorTest.php
@@ -0,0 +1,54 @@
+<?php
+
+use Kata\Homeworks\H06RegistrationApi\Validator;
+
+class ValidatorTest
+{
+    const USERNAME_VALID                = 'johndoe1';    
+    const USERNAME_TOO_SHORT            = 'doe';
+    const USERNAME_CAPITAL_LETTER      = 'JohnDoe';
+    const USERNAME_ONLY_CAPITAL_LETTERS = 'JOHNDOE';
+    const USERNAME_SPECIAL_CHARACTER   = 'johndoe1_';
+
+    const RESULT_OK    = true;
+    const RESULT_ERROR = false;
+    
+    
+    private $validator;
+    
+    public function setUp()
+    {
+        $this->validator = new Validator();
+    }
+    
+    /**
+     * @dataProvider isValidUsernameProvider
+     * 
+     * @param bool $expectedResult
+     * @param string $username
+     */
+    public function testIsValidUsername($expectedResult, $username)
+    {
+        $this->assertEquals($expectedResult, $this->validator->usernameCheck($username));
+    }
+    
+    
+    public function testIsValidPassword()
+    {
+        
+    }
+    
+    /** Data providers */
+    
+    public function isValidUsernameProvider()
+    {
+        return array(
+            array(self::RESULT_OK, self::USERNAME_VALID),
+            array(self::RESULT_ERROR, self::USERNAME_CAPITAL_LETTER),
+            array(self::RESULT_ERROR, self::USERNAME_ONLY_CAPITAL_LETTERS),
+            array(self::RESULT_ERROR, self::USERNAME_SPECIAL_CHARACTER),
+            array(self::RESULT_ERROR, self::USERNAME_TOO_SHORT),
+            array(self::RESULT_ERROR, str_repeat(self::USERNAME_VALID, 15)),
+        );
+    }
+}
