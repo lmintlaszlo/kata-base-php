6d4f650bd906978db8b78cdb4d58be41dba74e88
lmintlaszlo <laszlo.schumann@doclerholding.com>
2014-11-17 23:45:00 +0100

    Validator isValidPassword production

--- src/Homeworks/H06RegistrationApi/Validator.php
+++ src/Homeworks/H06RegistrationApi/Validator.php
@@ -4,10 +4,19 @@ namespace Kata\Homeworks\H06RegistrationApi;
 
 class Validator
 {
-    const REGEX_USERNAME = "/[0-9a-z]{4,128}/";
+    const REGEX_USERNAME = "/^[0-9a-z]{4,128}$/";
+    const REGEX_PASSWORD = "/[0-9a-zA-Z_]{6,}/";   //Mindnefele specialis bizbasz
     
     public function isValidUsername($username)
     {
-        return preg_match("/^[0-9a-z]{4,128}$/", $username);
+        return preg_match(self::REGEX_USERNAME, $username);
+    }
+    
+    public function isValidPassword($password, $passwordConfirm)
+    {
+        $passwordsMatch = ($password == $passwordConfirm);
+        $passwordsValid = preg_match(self::REGEX_PASSWORD, $password);
+        
+        return ($passwordsMatch && $passwordsValid);
     }
 }
