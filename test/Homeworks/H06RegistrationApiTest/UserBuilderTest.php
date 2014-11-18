b002f0b9ce312498e21aeb05e6b193550158cb42
lmintlaszlo <laszlo.schumann@doclerholding.com>
2014-11-18 22:40:20 +0100

    UserBuilder buildFromUsernameAndPass productuion

--- test/Homeworks/H06RegistrationApiTest/UserBuilderTest.php
+++ test/Homeworks/H06RegistrationApiTest/UserBuilderTest.php
@@ -21,10 +21,29 @@ class UserBuilderTest extends \PHPUnit_Framework_TestCase
     {
         $user = $this->userBuilder->buildFromUsernameAndPass($username, $password);
         
-        $this->assertInstanceOf('Kata\Homeworks\H06RegistrationApi\User', $user);
-        $this->assertEquals($username, $user->username, 'Username is not the same.');
-        $this->assertEquals($password, $user->passwordPlain, 'Password is not the same.');
-        $this->assertRegExp('/[a-z0-9]{32}/', $user->passwordHash, 'Passwordhash is not MD5 hash.');
+        $this->assertInstanceOf(
+            'Kata\Homeworks\H06RegistrationApi\User',
+            $user,
+            'Generated is not instance of User'
+        );
+        
+        $this->assertEquals(
+            $username,
+            $user->username,
+            'Username is not the same.'
+        );
+        
+        $this->assertEquals(
+            $password,
+            $user->passwordPlain,
+            'Password is not the same.'
+        );
+        
+        $this->assertRegExp(
+            '/[a-z0-9]{32}/',
+            $user->passwordHash,
+            'Passwordhash is not MD5 hash.'
+        );
     }
     
     
@@ -32,8 +51,8 @@ class UserBuilderTest extends \PHPUnit_Framework_TestCase
     public function buildFromUsernameAndPass()
     {
         return array(
-            array('John Doe',      'joe33'),
-            array('Boyce Goodkap', 'boYce5'),
+            array('John Doe',         'joe33'),
+            array('Boyce Goodkap',    'boYce5'),
             array('Leila Bimclecker', 'bImLei1758'),
         );
     }
