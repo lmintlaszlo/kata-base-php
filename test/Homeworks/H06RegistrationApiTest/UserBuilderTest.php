197f499ef36c343f2c18b054d81759677cbbef41
lmintlaszlo <laszlo.schumann@doclerholding.com>
2014-11-18 22:45:54 +0100

    UserBuilder buildFromUsername test

--- test/Homeworks/H06RegistrationApiTest/UserBuilderTest.php
+++ test/Homeworks/H06RegistrationApiTest/UserBuilderTest.php
@@ -17,7 +17,7 @@ class UserBuilderTest extends \PHPUnit_Framework_TestCase
      * @param type $username
      * @param type $password
      */
-    public function testBuildFromUsernameAndPass($username, $password)
+    public function testBuildFromUsernameAndPassword($username, $password)
     {
         $user = $this->userBuilder->buildFromUsernameAndPass($username, $password);
         
@@ -47,13 +47,52 @@ class UserBuilderTest extends \PHPUnit_Framework_TestCase
     }
     
     
+    public function testBuildFromUsername($username)
+    {
+        $user = $this->userBuilder->buildFromUsername($username);
+        
+        $this->assertInstanceOf(
+            'Kata\Homeworks\H06RegistrationApi\User',
+            $user,
+            'Generated is not instance of User'
+        );
+        
+        $this->assertEquals(
+            $username,
+            $user->username,
+            'Username is not the same.'
+        );
+        
+        $this->assertNotEmpty(
+            $user->passwordPlain,
+            'Password is not generated.'
+        );
+        
+        $this->assertRegExp(
+            '/[a-z0-9]{32}/',
+            $user->passwordHash,
+            'Passwordhash is not MD5 hash.'
+        );
+    }
+    
+    
     /** Data providers */
+    
     public function buildFromUsernameAndPass()
     {
         return array(
             array('John Doe',         'joe33'),
             array('Boyce Goodkap',    'boYce5'),
             array('Leila Bimclecker', 'bImLei1758'),
+        );
+    }
+    
+    public function buildFromUsername()
+    {
+        return array(
+            array('John Doe'),
+            array('Boyce Goodkap'),
+            array('Leila Bimclecker'),
         );
     }
 }
