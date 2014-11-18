bfaf22e74cfeaef77ca1286883db57e6d9b844f1
lmintlaszlo <laszlo.schumann@doclerholding.com>
2014-11-18 23:31:52 +0100

    Create Controller test and prod

---
+++ src/Homeworks/H06RegistrationApi/Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Kata\Homeworks\H06RegistrationApi;
+
+class Controller
+{
+    public function registration()
+    {
+        
+    }
+    
+    public function autoRegistration()
+    {
+        
+    }
+}
---
+++ test/Homeworks/H06RegistrationApiTest/ControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Kata\Homeworks\H06RegistrationApi\Controller;
+
+class ControllerTest 
+{
+    private $controller;
+    
+    public function setUp()
+    {
+        $this->controller = new Controller();
+    }
+    
+    public function testRegistration()
+    {
+        
+    }
+    
+    public function testAutoRegistration()
+    {
+        
+    }
+    
+}
