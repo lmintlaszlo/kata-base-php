04f13d33c58103b4808ebf139a5e29bd0094db4b
laptop <laptop@192.168.210.53>
2015-01-08 17:05:38 +0100

    Create files and first test assertTrue

---
+++ test/Lessons/L06Exam/StringCalculatorTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use Kata\Lessons\L06Exam\StringCalculator;
+
+
+class StringCalculatorTest extends PHPUnit_Framework_TestCase
+{
+    public function testAdd()
+    {
+        $this->assertTrue(true);
+    }
+}
